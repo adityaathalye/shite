f62dc165d63987264a78d6c8441d75c5ff15f02d
Aditya Athalye <aditya.athalye@gmail.com>
2022-05-09 20:07:29 +0530

    IT'S ALIVE. BUT Emacs edits don't hotreload. Vim does. WTF.

--- bin/hotreload.sh
+++ bin/hotreload.sh
@@ -47,16 +47,16 @@
 
 __shite_detect_changes() {
     # Continuously monitor the target directory for file CRUD events
-    # Emit a CSV record of UNIX_EPOCH_SECONDS,EVENT_TYPE,FILE_PATH
-    local dir_path="${1:-$(pwd)/public}"
-    local file_types="${2:-'html,js,css'}"
+    # Emit a CSV record of UNIX_EPOCH_SECONDS,EVENT_TYPE,DIR_PATH,FILE_NAME
+    local watch_dir="$(realpath -e ${1:-$(pwd)})"
+    local watch_events=${2:-'create,modify,delete,moved_to'}
 
     # WATCH A DIRECTORY
     inotifywait -m -r --exclude '/\.git/|/\.#|/#|.*(swp|swx|\~)$' \
                 --timefmt "%s" \
-                --format '%T,%:e,%w%f' \
-                -e 'create,modify,delete,moved_to' \
-                ${dir_path} |
+                --format '%T,%:e,%w,%f' \
+                -e ${watch_events} \
+                ${watch_dir} |
         # INCLUDE FILES
         # We have to grep, but a new version of inotifywait has an 'include'
         # option, which should obviate grep. So says Stackoverflow. Also we
@@ -77,83 +77,75 @@ __shite_distinct_events() {
     stdbuf -oL awk 'BEGIN { FS = "," } {if(!seen[$0]++ && seen[$1]++) print}'
 }
 
-__shite_test_actions() {
-    cp -f public/index.html public/deleteme.html
-    echo "foo" >> public/deleteme.html
-    mv public/deleteme.html public/deleteme2.html
-    rm public/deleteme2.html
-    # Should produce these "distinct" events:
-    # 1652008576,MODIFY,/home/adi/src/github/adityaathalye/shite/public/deleteme.html
-    # 1652008576,MOVED_TO,/home/adi/src/github/adityaathalye/shite/public/deleteme2.html
-    # 1652008576,DELETE,/home/adi/src/github/adityaathalye/shite/public/deleteme2.html
+__shite_xdo_cmd_browser_refresh() {
+    local window_id=${1:?"Fail. We expect window ID to be set in scope."}
+
+    printf "%s\n" \
+           "key --window ${window_id} --clearmodifiers 'F5'"
 }
 
-__shite_test_events() {
-    cat <<EOF
-1651991204,CREATE,/home/adi/src/github/adityaathalye/shite/public/deleteme.html
-1651991204,MODIFY,/home/adi/src/github/adityaathalye/shite/public/deleteme.html
-1651991204,MODIFY,/home/adi/src/github/adityaathalye/shite/public/deleteme.html
-1651991204,MOVED_TO,/home/adi/src/github/adityaathalye/shite/public/deleteme2.html
-1651991204,DELETE,/home/adi/src/github/adityaathalye/shite/public/deleteme2.html
-1651991221,CREATE,/home/adi/src/github/adityaathalye/shite/public/deleteme.html
-1651991221,MODIFY,/home/adi/src/github/adityaathalye/shite/public/deleteme.html
-1651991221,MODIFY,/home/adi/src/github/adityaathalye/shite/public/deleteme.html
-EOF
+__shite_xdo_cmd_goto_url() {
+    local window_id=${1}
+    local url=${2}
+
+    printf "%s\n" \
+           "key --window ${window_id} --clearmodifiers 'ctrl+l'" \
+           "type --window ${window_id} --clearmodifiers --delay 1 ${url}" \
+           "key --window ${window_id} --clearmodifiers 'Return'"
 }
 
+__shite_xdo_cmd_gen() {
+    local window_id=${1:?"Fail. We expect a window ID."}
+    local base_url=${2}
 
-__shite_event_cmd() {
-    # Write-ahead log of event_type data, and commands to execute
-    while IFS=',' read -r timestamp event_type filepath
+    while IFS=',' read -r timestamp event_type dir_path file_name
     do
-        case ${event_type} in
-            MODIFY )
-                echo "${event_type} implies Reload page ${filepath}"
+        # Emit xdotool commands based on the event / filename combination
+        case "${event_type}:${file_name}" in
+            MODIFY\:*.html )
+                # Reload when any content file is modified
+                __shite_xdo_cmd_browser_refresh ${window_id}
                 ;;
-            CREATE|MOVED_TO )
-                echo "${event_type} implies Goto new page ${filepath}"
+            CREATE|MOVED_TO\:*.html )
+                # GOTO newly-created or renamed content file
+                __shite_xdo_cmd_goto_url ${window_id} "${base_url:-file://${dir_path%\/}}/${file_name}"
                 ;;
-            DELETE )
-                echo "${event_type} implies Goto home page ${filepath}"
+            DELETE\:*.html )
+                # Fall back to home page when content file is deleted
+                __shite_xdo_cmd_goto_url ${window_id} "${base_url:-file://${dir_path%\/}}"
                 ;;
             * )
-                echo "${event_type} implies No-op ${filepath}"
+                # Reload page for any action on non-content pages (presumably static assets)
+                __shite_xdo_cmd_browser_refresh ${window_id}
                 ;;
         esac
     done
 }
 
-__shite_xdo_cmd_get_window() {
-    local tab_name=${1:?"Fail. We want to target a single specific tab only."}
-    local browser_name=${2:-"Mozilla Firefox"}
-
-    printf "%s\n" \
-           "search --onlyvisible --name \"${tab_name}.*${browser_name}$\""
-}
-
-__shite_xdo_cmd_browser_refresh() {
-    local window_id=${1:?"Fail. We expect window ID to be set in scope."}
-    printf "%s\n" \
-           "key_path --window ${window_id} --clearmodifiers 'F5'"
-}
-
-__shite_xdo_cmd_goto_url() {
-    local window_id=${1:?"Fail. We expect window ID to be set in scope."}
-    local url=${2:-"http://localhost:1313"}
-    printf "%s\n" \
-           "key_path --window ${window_id} --clearmodifiers 'ctrl+l'" \
-           "type --window ${window_id} --clearmodifiers --delay 1 \"${url}\"" \
-           "key_path --window ${window_id} --clearmodifiers 'Return'"
-}
-
 __shite_xdo_cmd_exec() {
-    # Presumably receives legal xdotool commands at stdin.
-    # Empty lines imply no-op.
-    local command_fn=${1:?"Fail. We expect a command function."}
-    shift
-    local command_fn_args="${@}"
-
-    ${command_fn} "${command_fn_args}" |
-        grep -v '^$' |
-        xdotool -
+    stdbuf -oL grep -v '^$' | xdotool -
 }
+
+__shite_xdo_cmd_exec_debug_log() {
+    tee >(1>&2 cat -)
+}
+
+shite_hotreload() {
+    local watch_dir=${1:?"Fail. Please specify a directory to watch"}
+    local tab_name=${2:?"Fail. We want to target a single specific tab only."}
+    local browser_name=${3:-"Mozilla Firefox"}
+    local base_url=${4:-""}
+
+    # Lookup window ID
+    local window_id=$(xdotool search --onlyvisible --name "${tab_name}.*${browser_name}$")
+
+    # Run pipeline
+    __shite_detect_changes ${watch_dir} |
+        __shite_distinct_events |
+        __shite_xdo_cmd_exec_debug_log |
+        __shite_xdo_cmd_gen ${window_id} ${base_url} |
+        __shite_xdo_cmd_exec_debug_log |
+        __shite_xdo_cmd_exec
+}
+
+shite_hotreload "./public" 'A static site'
