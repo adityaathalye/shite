6c2c327e533bc45bde3d2463206b8fc440ecae13
Aditya Athalye <aditya.athalye@gmail.com>
2022-05-10 16:42:55 +0530

    Fix, enhance hotreload cases, dispatch logic, debugging
    We want to dispatch on some combination of event type, file type, and
    whether the just-edited file is different from the last one. Different
    combinations imply different actions. Maybe a reload is sufficient, or
    maybe we should navigate to some page.
    
    It also turns out that different editing tools cause different inotify
    events for the same kind of action (e.g. modify, or rename). Vim and
    Emacs couldn't be more different. cp and sed are also subtly different
    from each other, and from the editors.

--- bin/hotreload.sh
+++ bin/hotreload.sh
@@ -49,13 +49,13 @@ __shite_detect_changes() {
     # Continuously monitor the target directory for file CRUD events
     # Emit a CSV record of UNIX_EPOCH_SECONDS,EVENT_TYPE,DIR_PATH,FILE_NAME
     local watch_dir="$(realpath -e ${1:-$(pwd)})"
-    local watch_events=${2:-'create,modify,delete,moved_to'}
+    local watch_events=${2}
 
     # WATCH A DIRECTORY
     inotifywait -m -r --exclude '/\.git/|/\.#|/#|.*(swp|swx|\~)$' \
                 --timefmt "%s" \
                 --format '%T,%:e,%w,%f' \
-                -e ${watch_events} \
+                $([[ -n ${watch_events} ]] && printf "%s %s" "-e" ${watch_events})  \
                 ${watch_dir} |
         # INCLUDE FILES
         # We have to grep, but a new version of inotifywait has an 'include'
@@ -95,35 +95,77 @@ __shite_xdo_cmd_goto_url() {
 }
 
 __shite_xdo_cmd_gen() {
+    # Generate xdotool commands based on information about the
+    # event type, file type, and whether the file has changed.
     local window_id=${1:?"Fail. We expect a window ID."}
     local base_url=${2}
+    local file_type
+    local file_status
+    local prev_file_name
 
     while IFS=',' read -r timestamp event_type dir_path file_name
     do
-        # Emit xdotool commands based on the event / filename combination
-        case "${event_type}:${file_name}" in
-            MODIFY\:*.html )
-                # Reload when any content file is modified
+        file_type="${file_name#*\.}"
+
+        file_status=$(
+            if [[ ${file_name} == ${prev_file_name} ]]
+            then printf "%s" "SAMEFILE"
+            else printf "%s" "NEWFILE"
+            fi
+                   )
+
+        # First catch "content" pages, which require special casing.
+        # At last, catch all non-content pages and do the only sane
+        # thing for anything done to those pages; viz. reload the site.
+        case "${event_type}:${file_type}:${file_status}" in
+            # RELOAD
+            # - When any content file is modified
+            # - When any non-current content file is deleted
+            #   (because that may affect the current page)
+            MODIFY:html:SAMEFILE ) ;& # catch vim edits
+            CLOSE_WRITE:CLOSE:html:SAMEFILE ) ;& # catch emacs edits
+            DELETE:html:NEWFILE )
                 __shite_xdo_cmd_browser_refresh ${window_id}
                 ;;
-            CREATE|MOVED_TO\:*.html )
-                # GOTO newly-created or renamed content file
-                __shite_xdo_cmd_goto_url ${window_id} "${base_url:-file://${dir_path%\/}}/${file_name}"
+            # GOTO - NAVIGATE
+            # - Newly-created content file, or
+            # - Moved/renamed content file
+            MODIFY:html:NEWFILE ) ;& # vim new file
+            CLOSE_WRITE:CLOSE:html:NEWFILE ) ;& # emacs new file
+            CREATE:html:* ) ;&
+            MOVED_TO:html:* )
+                __shite_xdo_cmd_goto_url \
+                    ${window_id} \
+                    "${base_url:-file://${dir_path%\/}}/${file_name}"
                 ;;
-            DELETE\:*.html )
-                # Fall back to home page when content file is deleted
-                __shite_xdo_cmd_goto_url ${window_id} "${base_url:-file://${dir_path%\/}}"
+            # GOTO - FALLBACK
+            # - home page when the current content file is deleted
+            DELETE:html:SAMEFILE )
+                __shite_xdo_cmd_goto_url \
+                    ${window_id} \
+                    "${base_url:-file://${dir_path%\/}}"
                 ;;
+
+            # RELOAD page for any action on non-content pages,
+            # presumably static assets.
             * )
-                # Reload page for any action on non-content pages (presumably static assets)
                 __shite_xdo_cmd_browser_refresh ${window_id}
                 ;;
         esac
+
+        # Remember the file for the next cycle
+        prev_file_name=${file_name}
     done
 }
 
 __shite_xdo_cmd_exec() {
-    stdbuf -oL grep -v '^$' | xdotool -
+    local cmd=$(
+        if [[ ${SHITE_DEBUG} == "debug" ]]
+        then "cat"
+        else "xdotool"
+        fi
+          )
+    stdbuf -oL grep -v '^$' | $cmd -
 }
 
 __shite_xdo_cmd_exec_debug_log() {
@@ -131,6 +173,7 @@ __shite_xdo_cmd_exec_debug_log() {
 }
 
 shite_hotreload() {
+    # Maybe improve with getopts later
     local watch_dir=${1:?"Fail. Please specify a directory to watch"}
     local tab_name=${2:?"Fail. We want to target a single specific tab only."}
     local browser_name=${3:-"Mozilla Firefox"}
@@ -140,7 +183,9 @@ shite_hotreload() {
     local window_id=$(xdotool search --onlyvisible --name "${tab_name}.*${browser_name}$")
 
     # Run pipeline
-    __shite_detect_changes ${watch_dir} |
+    # - Events of interest 'create,modify,close_write,moved_to,delete'
+    __shite_detect_changes \
+        ${watch_dir} 'create,modify,close_write,moved_to,delete' |
         __shite_distinct_events |
         __shite_xdo_cmd_exec_debug_log |
         __shite_xdo_cmd_gen ${window_id} ${base_url} |
@@ -148,4 +193,8 @@ shite_hotreload() {
         __shite_xdo_cmd_exec
 }
 
-shite_hotreload "./public" 'A static site'
+__shite_debug_run() {
+    SHITE_DEBUG="debug" shite_hotreload \
+               "./public" 'A static' \
+               > /dev/null
+}
