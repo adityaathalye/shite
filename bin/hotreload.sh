db56cde8e699fe16c1949e343af9966ed6c7a189
Aditya Athalye <aditya.athalye@gmail.com>
2022-05-16 14:26:21 +0530

    Add convenience logging functions which we can switch
    on or off based on a debug flag.

--- bin/hotreload.sh
+++ bin/hotreload.sh
@@ -48,6 +48,16 @@
 # c.f https://www.perkin.org.uk/posts/how-to-fix-stdio-buffering.html
 # ##################################################
 
+__to_stderr() {
+    # Ref: I/O Redirection: http://tldp.org/LDP/abs/html/io-redirection.html
+    if [[ ${SHITE_DEBUG} == "debug" ]]
+    then 1>&2 printf "%s\n" "$(date --iso-8601=seconds) $@"
+    fi
+}
+
+__log_info() {
+    __to_stderr "$(echo "INFO $0 $@")"
+}
 
 __shite_tap_stream() {
     tee >(1>&2 cat -)
